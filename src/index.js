873c5b6b9524ef80a6bbbcb9bfddbbd1e1b35e01
Tetyana Solyanyk <tanyas81@gmail.com>
2025-05-26 21:07:36 +0300

    add footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { WeatherSearchEngine } from "./WeatherSearch";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +10,31 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <WeatherSearchEngine />
+    <footer>
+      {" "}
+      <a
+        href="https://github.com/TattiS/weather-react.git"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open-source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://github.com/TattiS"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        TattiS
+      </a>{" "}
+      at{" "}
+      <a
+        href="https://www.shecodes.io/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        SheCodes
+      </a>
+    </footer>
   </StrictMode>
 );
